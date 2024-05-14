68eef668739eb149f2f8dcda15f8ab2c38688344
kenyafan <kenyafan321@gmail.com>
2024-05-14 21:50:33 +0300

    up

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import s from "./Home.module.css";
 
 const Home = () => {
-  return <div className={s.container}></div>;
+  return (
+    <div className={s.container}>
+      <h1>Welcome to Car Rental</h1>
+      <img
+        className={s.img}
+        src="https://www.hertz.com/content/dam/hertz/global/blog-articles/resources/car-rental-vs-car-sharing/car-keys.jpg"
+        alt="car-keys"
+      />
+    </div>
+  );
 };
 
 export default Home;
