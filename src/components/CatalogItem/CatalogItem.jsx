1f6d98184212d58f25b0f5ac30c5bd668fcda5d2
kenyafan <kenyafan321@gmail.com>
2024-05-14 11:50:53 +0300

    up

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -1,4 +1,5 @@
 import { useToggle } from "../../hooks/useToggle";
+import { Icon } from "../../image/Icon";
 import Modal from "../Modal/Modal";
 import s from "./CatalogItem.module.css";
 
@@ -24,14 +25,15 @@ function CatalogItem({ item }) {
     <li className={s.item}>
       <>
         <img className={s.img} src={img} alt={model} width="274" height="268" />
-        <div>
-          <div className={s.wrapper}>
-            <h3 className={s.title}>
-              {make} <span className={s.model}>{model}</span>, {year}
-            </h3>
-            <p className={s.price}>{rentalPrice}</p>
-          </div>
+        <Icon id="favorite" size={24} className={s.icon_favorite} />
+
+        <div className={s.wrapper}>
+          <h3 className={s.title}>
+            {make} <span className={s.model}>{model}</span>, {year}
+          </h3>
+          <p className={s.price}>{rentalPrice}</p>
         </div>
+
         <div>
           <p className={s.text}>
             {addressWithoutStreet} | {rentalCompany} | {type} | {model} |{" "}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import s from "./Modal.module.css";
+import { Icon } from "../../image/Icon.jsx";
 
 import { useCallback, useEffect } from "react";
 
@@ -10,7 +11,6 @@ const Modal = ({ item, closeModal }) => {
     year,
     rentalPrice,
     address,
-
     type,
     mileage,
     functionalities,
@@ -21,6 +21,17 @@ const Modal = ({ item, closeModal }) => {
     accessories,
     rentalConditions,
   } = item;
+
+  let addressParts = address.split(", ");
+  let addressWithoutStreet = addressParts.slice(1).join(", ");
+
+  const rentalConditionsArray = rentalConditions.split("\n");
+  const conditionAge = rentalConditionsArray[0];
+  const conditionLicense = rentalConditionsArray[1];
+  const conditionRequired = rentalConditionsArray[2];
+
+  const ageMatch = conditionAge.match(/\d+/);
+  const age = ageMatch ? ageMatch[0] : null;
 
   const handleKeyDown = useCallback(
     (e) => {
@@ -47,36 +58,50 @@ const Modal = ({ item, closeModal }) => {
 
   return (
     <div className={s.wrapper} onClick={handleBackdropClick}>
-      <div className={s.content}>
+      <div className={s.modal}>
         <button onClick={closeModal} className={s.closeBtn}>
-          X
+          <Icon id="close" size={20} className={s.closeIcon} />
         </button>
         <img className={s.img} src={img} alt={model} width="461" height="248" />
-        <div className={s.top_wrapper}>
+        <div className={s.info_wrapper}>
           <div className={s.title_wrapper}>
-            <h3>
-              {make} {model}, {year}
+            <h3 className={s.title}>
+              {make} <span className={s.model}>{model}</span>, {year}
             </h3>
-            <p>{rentalPrice}</p>
           </div>
-          <p>
-            {address} | Id: {id} | Year: {year} | Type: {type} | Fuel
-            consumption: {fuelConsumption} | Engine Size: {engineSize}
+          <p className={s.text}>
+            {addressWithoutStreet} | Id: {id} | Year: {year} | Type: {type} |
+            Fuel consumption: {fuelConsumption} | Engine Size: {engineSize}
           </p>
-          <p>{description}</p>
+          <p className={s.description}>{description}</p>
         </div>
-        <div>
-          <h3>Accessories and functionalities:</h3>
-          <p>
-            {functionalities.join(" | ")} | {accessories.join(" | ")}
+        <div className={s.accessories_wrapper}>
+          <h3 className={s.title_accessories}>
+            Accessories and functionalities:
+          </h3>
+          <p className={s.text_accessories}>
+            {functionalities.join(" | ")} <br></br> {accessories.join(" | ")}
           </p>
         </div>
-        <div>
-          <h3>Rental Conditions:</h3>
-          <p>
-            {rentalConditions.split("/n")} Mileage: {mileage} Price:
-            {rentalPrice}
-          </p>
+        <div className={s.conditions_wrapper}>
+          <h3 className={s.title_accessories}>Rental Conditions:</h3>
+          <div className={s.conditions_inner}>
+            <div className={s.conditions_flex}>
+              <p>
+                Minimum age: <span className={s.age}>{age}</span>
+                {conditionLicense}
+              </p>
+            </div>
+            <div className={s.conditions_flex}>
+              <p>{conditionRequired}</p>
+              <p>
+                Mileage:<span>{mileage}</span>
+              </p>
+              <p>
+                Price:<span>{rentalPrice}</span>
+              </p>
+            </div>
+          </div>
         </div>
         <a href="tel:+380730000000" className={s.btn}>
           Rental car
